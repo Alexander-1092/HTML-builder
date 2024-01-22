49342db219ff5eaa12e61547f770d63ce1697b21
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2024-01-22 20:29:36 +0300

    feat: add parting + comments

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,25 +2,27 @@ const fs = require("fs");
 const path = require("path");
 const { stdin, stdout } = process;
 
-stdout.write("Hi! Enter text:\n");
+stdout.write("Hi! Enter text:\n");//We display a greeting
 stdin.on("data", (data) => {
-  let str = '';
-  str = data;
-	const pathFile = path.join(__dirname, '02-write-file');
-	if (pathFile) {
+	const pathFile = path.join(__dirname, '02-write-file'); // The path to our file where the data is saved
+	if (pathFile) { // If the file already exists, then add it
 		fs.appendFile(
 			path.join(__dirname, '02-write-file'),
-			str,
-			(err) => {
+			data, //Inserting the received data
+			(err) => { //Check in case of error
 				if(err) throw err
 		})	
-	} else {
+	} else { // If the file does not exist, then create it
 		fs.writeFile(
 			path.join(__dirname, '02-write-file'),
-			str,
+			data,
 			(err) => {
 				if(err) throw err
 		})
-	}
+	};
 });
 
+process.on('SIGINT', function() {//catching the completion event
+	stdout.write('Bye');
+	process.exit();
+})
