2552c9344e0212bdb6216c8854e3d2b5c8828029
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2024-01-22 18:01:44 +0300

    feat: add coments

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const fs = require('fs');
-const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8')
-let data = ''
 
-stream.on("data", (chunk) => (data += chunk));
-stream.on("end", () => console.log(data));
-stream.on("error", (error) => console.log("Error", error.message));
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8') // indicate the path
+let data = '' //we connect parts of the text in a variable
+
+stream.on("data", (chunk) => (data += chunk)); //passing parts of the text to a variable
+stream.on("end", () => console.log(data)); // we end the event and output everything in the variable
+stream.on("error", (error) => console.log("Oops, error", error.message)); //error event
