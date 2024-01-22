f2442064f4ec52c4530b837343d5f0e6a91982f4
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2024-01-22 21:15:45 +0300

    refactor: add eslint

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const fs = require('fs');
 
-const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8') // indicate the path
-let data = '' //we connect parts of the text in a variable
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8'); // indicate the path
+let data = ''; //we connect parts of the text in a variable
 
-stream.on("data", (chunk) => (data += chunk)); //passing parts of the text to a variable
-stream.on("end", () => console.log(data)); // we end the event and output everything in the variable
-stream.on("error", (error) => console.log("Oops, error", error.message)); //error event
+stream.on('data', (chunk) => (data += chunk)); //passing parts of the text to a variable
+stream.on('end', () => console.log(data)); // we end the event and output everything in the variable
+stream.on('error', (error) => console.log('Oops, error', error.message)); //error event
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,28 +1,26 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const { stdin, stdout } = process;
 
-stdout.write("Hi! Enter text:\n");//We display a greeting
-stdin.on("data", (data) => {
-	const pathFile = path.join(__dirname, '02-write-file'); // The path to our file where the data is saved
-	if (pathFile) { // If the file already exists, then add it
-		fs.appendFile(
-			path.join(__dirname, '02-write-file'),
-			data, //Inserting the received data
-			(err) => { //Check in case of error
-				if(err) throw err
-		})	
-	} else { // If the file does not exist, then create it
-		fs.writeFile(
-			path.join(__dirname, '02-write-file'),
-			data,
-			(err) => {
-				if(err) throw err
-		})
-	};
+stdout.write('Hi! Enter text:\n'); //We display a greeting
+stdin.on('data', (data) => {
+  const pathFile = path.join(__dirname, '02-write-file'); // The path to our file where the data is saved
+  if (pathFile) {
+    fs.appendFile(
+      path.join(__dirname, '02-write-file'),
+      data, //Inserting the received data
+      (err) => {
+        if (err) throw err;
+      },
+    );
+  } else {
+    fs.writeFile(path.join(__dirname, '02-write-file'), data, (err) => {
+      if (err) throw err;
+    });
+  }
 });
 
-process.on('SIGINT', function() {//catching the completion event
-	stdout.write('Bye');
-	process.exit();
-})
+process.on('SIGINT', () => {
+  stdout.write('Bye');
+  process.exit();
+});
